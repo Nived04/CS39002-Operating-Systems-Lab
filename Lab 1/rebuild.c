e99797962f31b8a31a04b1a927e681c69a39b787
Nived04 <shahnived04@gmail.com>
2025-01-08 19:39:24 +0530

    temp

--- Lab 1/rebuild.c
+++ Lab 1/rebuild.c
@@ -4,11 +4,14 @@
 #include <string.h>
 #include <sys/wait.h>
 
-const int MAX_DEP = 100;
+const int MAX_DEP = 500;
+const char* dag_file = "foodep.txt";
+const char* visited_file = "done.txt";
 
 int get_dependencies(const char* file, int foodule, int dependencies[]) {
-    FILE* f;
-    f = fopen(file, "r");
+    FILE* f = fopen(file, "r");
+
+    // jump to the line that contains the dependencies of the argument foodule
     int cnt = 0;
     while(cnt != foodule) {
         if(fgetc(f) == '\n')
@@ -19,14 +22,17 @@ int get_dependencies(const char* file, int foodule, int dependencies[]) {
     }
 
     char line[MAX_DEP];
-    fgets(line, sizeof(line), f);
+    fgets(line, MAX_DEP, f);
     // printf("Line: %s", line);    
 
     fclose(f);
+
+    // after the colon, there will either be a space (if there are some dependencies) or newline (if no dependencies)
     if(line[1] < 48 || line[1] > 57) {
         return 0;
     }
 
+    // separate the dependencies and add them to the dependencies array for the current foodule
     int i=0;
     char *dep = strtok(line, " ");
     while(dep != NULL) {
@@ -35,24 +41,23 @@ int get_dependencies(const char* file, int foodule, int dependencies[]) {
         dep = strtok(NULL, " ");
         i++;
     }
+
+    // return the number of dependencies
     return i;
 }
-
-void rebuild_foodule(int node) {}
 
 int main(int argc, char* argv[]) {
     if(argc < 2) {
         printf("Please enter a foodule to rebuild\n");
-        exit(1);
+        exit(0);
     }
 
-    char* dag_file = "foodep.txt";
-    char* visited_file = "done.txt";
-
     FILE* fdag = fopen(dag_file, "r");
+    // first line of the foodep file contains the total number of foodules
     int total_foodules;
     fscanf(fdag, "%d", &total_foodules);
     
+    // if there is just 1 other argument, it means that it is the root process
     if(argc == 2) {
         FILE *fvf = fopen(visited_file, "w");
         char* vis = (char*)malloc((total_foodules + 1)*sizeof(char));
@@ -64,16 +69,14 @@ int main(int argc, char* argv[]) {
     }
 
     fclose(fdag);
+
     int foodule = atoi(argv[1]);
-
     int* dependencies = (int*)malloc(MAX_DEP*sizeof(int));
-
     int tot_dep = get_dependencies(dag_file, foodule, dependencies);
-
-    // printf("%d\n", tot_dep);
 
     for(int i=0; i<tot_dep; i++) {
         int child = dependencies[i];
+        
         FILE *fvf = fopen(visited_file, "r");
         char line[total_foodules + 1];
         fgets(line, sizeof(line), fvf);
