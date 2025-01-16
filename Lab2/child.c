923b82860df2a6f1ab847a7aaa960c0fb918dd64
Nived04 <shahnived04@gmail.com>
2025-01-16 15:34:07 +0530

    avoided pause-hanging and added comments

--- Lab2/child.c
+++ Lab2/child.c
@@ -9,10 +9,12 @@ const char* status[4] = {"CATCH", ".... ", "MISS ", "     "};
 
 // initially the child is playing
 int myStat = 1; 
-int passOnPid, n, curr_child;
+// store the total children, next child to send the print signal and the curr child number
+int n, passOnPid, curr_child;
 
 /* The signal handler for the child process */
 void childSigHandler (int sig) {
+    // on receiving sigint, either the child is out of game or the child is the winner 
     if(sig == SIGINT) {
         if(myStat == 1) {
             printf("+++ Child %d: Yay! I am the winner!\n", curr_child);
@@ -21,19 +23,23 @@ void childSigHandler (int sig) {
         exit(0);
     }
 
+    // on receiving sigusr1, print the status of the child and send sigusr1 to the next child
     if(sig == SIGUSR1) {
         printf("%s   ", status[myStat]);
         fflush(stdout);
 
+        // if it is the nth child, send the sigint interrupt to the dummy child so that parent can end waiting
         if(curr_child == n) {
             FILE* f = fopen("dummycpid.txt", "r");
             fscanf(f, "%d", &passOnPid);
             fclose(f);
         }
 
+        // if stat is "0(caught)", change to "playing(1)", else if stat is "2(missed)", change to "out of game(3)"
         if(myStat%2 == 0) myStat = myStat+1;
         kill (passOnPid, (curr_child == n) ? SIGINT : SIGUSR1);
     }
+    // on receiving sigusr2, change the status of the child to out of game with 20% probability
     else if(sig == SIGUSR2) {
         myStat = (rand()%10 < 2) ? 2 : 0;
         kill(getppid(), (myStat == 2) ? SIGUSR2 : SIGUSR1);
@@ -45,6 +51,7 @@ int main() {
 
     srand((unsigned int)time(NULL) ^ (getpid()<<16));
 
+    // reading the PIDs of sibling processes so as to find the PID of the next child in the circle
     FILE* f = fopen("childpid.txt", "r");
     fscanf(f, "%d", &n);
     fgetc(f);
@@ -64,6 +71,7 @@ int main() {
 
     fclose(f);
 
+    // registering signal handlers for SIGUSR1, SIGUSR2 and SIGINT
     signal(SIGUSR1, childSigHandler);
     signal(SIGUSR2, childSigHandler);
     signal(SIGINT, childSigHandler);
--- Lab2/parent.c
+++ Lab2/parent.c
@@ -7,9 +7,17 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 
-int throw_to, *childPIDs, n;
+// global variables to store next child to throw the ball and number of children
+int throw_to, n;
+// array to store PIDs of all children
+int *childPIDs;
+// array to store "playing or not" status of all children
 bool *child_status;
+// received status boolen to check if the parent has already received the signal from child 
+// so that it can avoid a "hanging" scenario, where the parent pauses but the child has already sent the signal
+bool received_signal = 0;
 
+// returns 1 if there are more than 1 children playing, 0 otherwise
 bool inPlay() {
     int cnt = 0, i;
     for(i=1; i<=n; i++) {
@@ -18,6 +26,7 @@ bool inPlay() {
     return (cnt == 1) ? 0 : 1;
 }
 
+// finds and returns the next child to pass the ball to (in circular order)
 int getNextPlayingChild(int curr) {
     int i=0; 
     while(i<n) {
@@ -29,8 +38,9 @@ int getNextPlayingChild(int curr) {
     return -1; // situation never arises
 }
 
+// function to print some additional formatting
 void formattedOutput(int bef) {
-    char s1[10 + 8*n], s2[11+8*n], temp[10];
+    char s1[11 + 8*n], s2[11 + 8*n], temp[10];
  
     memset(s1, ' ', sizeof(s1));  
     s1[sizeof(s1) - 1] = '\0';
@@ -58,8 +68,10 @@ void formattedOutput(int bef) {
     return;
 }
 
+// custom signal handler for SIGUSR2: which updates the status of the child to out of the game
 void signalHandler(int sig) {
-    if(sig == SIGUSR2) child_status[throw_to] = 0; // out of the game
+    if(sig == SIGUSR1 || sig == SIGUSR2) received_signal = 1;
+    if(sig == SIGUSR2) child_status[throw_to] = 0;
     return;
 }
 
@@ -68,9 +80,10 @@ int main(int argc, char* argv[]) {
         printf("Please enter a single argument denoting number of children\n");
         exit(1);
     }
-
+    // total number of children
     n = atoi(argv[1]);
-    
+
+    // registering signal handlers for SIGUSR1 and SIGUSR2
     signal(SIGUSR1, signalHandler);
     signal(SIGUSR2, signalHandler);
 
@@ -81,34 +94,36 @@ int main(int argc, char* argv[]) {
     fprintf(f, "%d\n", n);
     fflush(f);
 
+    // fork n children and store their PIDs in childPIDs array and in childpid.txt
     for(int i=0; i<n; i++) {
         int cpid = fork();
 
-        if(cpid) {
+        if(cpid) {  // Parent Process
             childPIDs[i+1] = cpid;
             fprintf(f, "%d\n", cpid);
             fflush(f);
         }
-        else {
-            char temp[12];
-            sprintf(temp, "%d", i+1);
+        else {      // Child Process
             execlp("./child", "./child", NULL);
-            perror("execlp");
             exit(1);
         }
     }
 
     fclose(f);
+
     printf("Parent: %d child processes created\n", n);
     printf("Parent: Waiting for child processes to read child database\n");
+
     sleep(2);
     fflush(stdout);
 
+    // initially all children are playing
     child_status = (bool *)malloc((n+1)*sizeof(bool));
     for(int i=0; i<n+1; i++) {
         child_status[i] = 1;
     }
 
+    // initially the ball is thrown to the first child
     throw_to = 1;
     int pc = inPlay(child_status, n);
 
@@ -116,9 +131,20 @@ int main(int argc, char* argv[]) {
     fflush(stdout);
 
     while(pc) {
+        // throw the ball to the next child
         kill(childPIDs[throw_to], SIGUSR2);
-        pause();
+        
+        /* 
+           wait for the child's signal signifying catch miss or catch success
+           in case the child sends the signal before the pause function is called
+           the received_signal flag will have been set to 1 and the control will flow 
+           without hanging 
+        */
+       
+        if(!received_signal) pause();
+        else received_signal = 0;
 
+        // fork a dummy child for allowing sequential status printing
         int dpid = fork();
         if(dpid) {
             FILE* fdc = fopen("dummycpid.txt", "w");
@@ -131,6 +157,7 @@ int main(int argc, char* argv[]) {
             printf("|    ");
             fflush(stdout);
 
+            // intiate status printing by sending SIGUSR1 to the first child
             kill(childPIDs[1], SIGUSR1);
             waitpid(dpid, NULL, 0);
 
@@ -141,7 +168,9 @@ int main(int argc, char* argv[]) {
             execlp("./dummy", "./dummy", NULL);
         }
 
+        // check if the game is over
         pc = inPlay();
+        // get the next child to throw the ball
         throw_to = getNextPlayingChild(throw_to);
     }
 
@@ -149,6 +178,7 @@ int main(int argc, char* argv[]) {
     formattedOutput(1);
     fflush(stdout);
 
+    // finally send SIGINT to all children to terminate them
     for(int i=1; i<=n; i++) {
         kill(childPIDs[i], SIGINT);
     }
